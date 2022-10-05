635321697a44b64b788733e43a0ec87cfeb9fbac
Roger Almeida <roger.eduardo@gmail.com>
2022-10-05 15:38:14 +1100

    :construction: DevOps talk

---
+++ pages/talks.tsx
@@ -0,0 +1,9 @@
+const Talks = () => {
+  return (
+    <ul>
+      <li><a href="/talks/devops-from-the-trenches/index.html"> DevOps from the trenches</a></li>
+    </ul>
+  )
+}
+
+export default Talks
